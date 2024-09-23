25d598b8589f6e678d5dfefd2fd86f4c626ad55f
Ehsan-saradar <Ehsan.Saradar@gmail.com>
2024-09-23 13:31:17 +0330

    Update index content

--- app/copy/NavBar.ts
+++ app/copy/NavBar.ts
@@ -19,7 +19,7 @@ export const navBarCopy = {
     ],
     pre_order: {
         name: "Pre-order Now",
-        url: "https://docs.google.com/forms/d/e/1FAIpQLSeXJc5fWtN6S2tkWM7A7SF5rPMyZTgZlH-pCzqr5xAlv9U2Tw/viewform?usp=sf_link",
-        target: "_blank"
+        url: "#pre-order",
+        target: "_self"
     },
 }
