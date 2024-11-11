883c7cb53f057ed598cacded4a147b7e0f27cb06
Ehsan-saradar <Ehsan.Saradar@gmail.com>
2024-11-11 13:06:23 +0330

    Update layout.tsx

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -9,20 +9,20 @@ import "@/styles/index.scss";
 export const metadata: Metadata = {
   title: "VultiPhone",
   description: "VultiPhone is a secure and easy-to-use platform that allows you to manage your digital assets.",
-  metadataBase: new URL("https://vultiphone.vercel.app/"),
+  metadataBase: new URL("https://vutiphone.com/"),
   keywords: ['VultiPhone', 'Blockchain', ' Earn Crypto with Security', 'Multi-Chain Vault', ' WEWE' , 'DEFI' , 'NFT'],
   authors: [{ name: 'Vultisig' }],
   openGraph: {
     siteName: 'VultiPhone',
-    url: 'https://vultiphone.vercel.app/',
-    images: 'https://vultiphone.vercel.app/thumbnails/home.png',
+    url: 'https://vutiphone.com/',
+    images: 'https://vutiphone.com/thumbnails/home.png',
   },
   twitter: {
     site: "VultiPhone",
     card: "summary_large_image",
     title: "VultiPhone",
     description: "VultiPhone is a secure and easy-to-use platform that allows you to manage your digital assets.",
-    images: 'https://vultiphone.vercel.app/thumbnails/home.png',
+    images: 'https://vutiphone.com/thumbnails/home.png',
   },
   icons: {
     icon: "logo.svg",
